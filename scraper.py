8fdba1914f0257130635aa068adc2878c64eca15
Einsi3dler <102097200+Einsi3dler@users.noreply.github.com>
2023-01-23 23:55:22 +0100

    file extension update

--- scraper.py
+++ scraper.py
@@ -43,20 +43,3 @@ for repo in repo_list:
         quickSort(arr, pi + 1, high);
     }
 }
- 
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
- 
-// Driver program to test above functions
-int main()
-{
-    int arr[] = {10, 7, 8, 9, 1, 5};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    quickSort(arr, 0, n-1);
-    printf("Sorted array: \n"
