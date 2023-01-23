e9e737e8fbde47b73558bd17449d910d024cc700
Einsi3dler <102097200+Einsi3dler@users.noreply.github.com>
2023-01-23 23:55:44 +0100

    old state reversal

--- scraper.py
+++ scraper.py
@@ -29,17 +29,4 @@ for repo in repo_list:
 
 
     
-    void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        /* pi is partitioning index, arr[p] is now
-           at right place */
-        int pi = partition(arr, low, high);
- 
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
+  
